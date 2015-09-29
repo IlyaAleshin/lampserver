dd9002bf791edfb8e86babd92cd01c3f68991518
Ilya Aleshin <ilya@ialeshin.ru>
2015-09-29 04:24:10 +0300

    fix github link

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='lampserver',
       version='0.1',
       description='best lamp server',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/IlyaAleshin/lampserver',
       author='Ilya Aleshin',
       author_email='ilya@ialeshin.ru',
       license='MIT',
